5284b453347938a89ce04f3eeb7395e38ee71e45
smriti-srivastava <smriti1155@gmail.com>
2018-07-26 04:47:21 +0530

    First Commit

---
+++ AddEmployee/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddEmployee
+{
+    public class EmptyQualification: Exception
+    {
+        public EmptyQualification() : base("EMPTY QUALIFICATION-QUALIFICATION CAN'T BE EMPTY.") { }
+    }
+    public class InvalidQualification : Exception
+    {
+        public InvalidQualification() : base("INVALID QUALIFICATION-PLEASE ENTER QUALIFICATION ONLY AS: BCA, BE, BSC, BCom, MCom OR CA.") { }
+    }
+    class Employee
+    {
+        private int _employeeID;
+        private string _qualification;
+        private string _department;
+        public Employee(int employeeID, string qualification)
+        {
+            this._employeeID = employeeID;
+            if (qualification.Equals("BE") || qualification.Equals("BCA") || qualification.Equals("BSC"))
+            {
+                this._department = "IT Department";
+                Console.WriteLine("EMPLOYEE ADDED UNDER IT DEPARTMENT");
+            }
+            else if (qualification.Equals("BCom") || qualification.Equals("MCom") || qualification.Equals("CA"))
+            {
+                this._department = "Accounts Department";
+                Console.WriteLine("EMPLOYEE ADDED UNDER ACCOUNTS DEPARTMENT");
+            }
+            else if (Qualification == "")
+            {
+                throw (new EmptyQualification());
+            }
+            else
+            {
+                throw (new InvalidQualification());
+            }
+            this._qualification = Qualification;
+        }
+        public string Qualification
+        {
+            get
+            {
+                return this._qualification;
+            }
+
+        }
+        public string Department
+        {
+            get
+            {
+                return this._department;
+            }
+
+        }
+
+    }
+}
---
+++ AddEmployee/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AddEmployee
+{
+    class Program
+    {
+        public delegate List<Employee> NewEmployeeList();
+
+        public static List<Employee> CreateNewEmployeeList()
+        {
+            return (new List<Employee>());
+        }
+        static void Main(string[] args)
+        {
+            int choice, employeeID, numberOfEmployee=0;
+            string qualification;
+            NewEmployeeList createList = new NewEmployeeList(CreateNewEmployeeList);
+            List<Employee> employees = createList();
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("\n\n------CHOICE MENU------");
+                    Console.WriteLine("1. ADD EMPLOYEE");
+                    Console.WriteLine("2. EXIT FROM HERE");
+                    Console.WriteLine("\nENTER YOUR CHOICE:");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("ENTER THE EMPLOYEE ID:");
+                            employeeID = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("ENTER THE EMPLOYEE'S QUALIFICATION: ");
+                            qualification = Console.ReadLine();
+                            employees.Add(new Employee(employeeID, qualification));
+                            numberOfEmployee++;
+                            break;
+                        case 2:
+                            System.Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("\n\n\n-------------WARNING-------------");
+                            Console.WriteLine("INVALID CHOICE ENTERED. TRY AGAIN\n\n\n");
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                using (StreamWriter sr = File.AppendText(@"D:\AddEmployeeExceptionLog.txt"))
+                {
+                    sr.WriteLine(e.ToString());
+                }
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                System.Environment.Exit(0);
+            }
+
+        }
+    }
+}
